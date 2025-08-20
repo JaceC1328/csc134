38ebd2180b24663d5d49da8c77edbffd4fd7eefd
JaceC1328 <caswellj1328@student.faytechcc.edu>
2025-08-20 15:39:11 +0000

    changed to talk about cars

---
+++ m1t1_hello.cpp
@@ -0,0 +1,16 @@
+/*
+CSC 134
+M1T1 - Hello World
+J Caswell
+8/20/25
+First program.
+*/
+//magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout <<"hello world" <<endl;
+    cout << "My foo is cars" <<endl;
+    return 0;
+}
