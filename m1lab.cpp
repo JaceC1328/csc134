bff591e0c2c6fea35af308527bc8a6b94ec73e7c
JaceC1328 <caswellj1328@student.faytechcc.edu>
2025-08-27 15:34:32 +0000

    fixed pricing and double checked

--- m1lab.cpp
+++ m1lab.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 int main() {
     // start program
-    //declare all of our vairable ("whiteboards")
+    //declare all of our variable ("whiteboards")
 string item_name = "tire"; // replace with your item
 int num_items = 10;
 double cost_per = 80.25;
@@ -23,7 +23,9 @@ cout << "Each " << item_name << " is $" <<cost_per << endl;
 cout << "We have " << num_items << " total." << "\n";
 // do the processing
 double total_cost = num_items * cost_per;
-
+// Print the output
+cout << "Buying them all will cost: $" << total_cost << endl;
+cout << "Thanks for shopping with us!" << endl;
 
     //end program
     return 0;
