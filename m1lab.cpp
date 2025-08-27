0d556039c8bdd93e025a00b5eb2c3dd564b45a4a
JaceC1328 <caswellj1328@student.faytechcc.edu>
2025-08-27 14:49:38 +0000

    added tire cost and number of tires in stock

--- m1lab.cpp
+++ m1lab.cpp
@@ -13,13 +13,17 @@ using namespace std;
 int main() {
     // start program
     //declare all of our vairable ("whiteboards")
-string item = "tire"; // replace with your item
+string item_name = "tire"; // replace with your item
 int num_items = 10;
-double cost_per = 0.25;
+double cost_per = 80.25;
 
-cout << "welcome to the " <<item<< " store!" <<endl;
+//give oue slaes pitch
+cout << "welcome to the " << item_name << " store!" << endl;
+cout << "Each " << item_name << " is $" <<cost_per << endl;
+cout << "We have " << num_items << " total." << "\n";
 // do the processing
 double total_cost = num_items * cost_per;
+
 
     //end program
     return 0;
