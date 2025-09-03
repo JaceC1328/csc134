6afc7806c82f8d56de6894a4fde0e539fbb2ac72
JaceC1328 <caswellj1328@student.faytechcc.edu>
2025-09-03 14:34:26 +0000

    Started

---
+++ M2T1.cpp
@@ -0,0 +1,16 @@
+/*
+csc 134 
+M2T1
+jacec
+9/3/25
+purpose: The "tire" sales program with user input
+*/
+
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
