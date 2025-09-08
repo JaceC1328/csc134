07376b3eb1485c1b281bf0f885ec4a9238a3b0a2
JaceC1328 <caswellj1328@student.faytechcc.edu>
2025-09-08 14:36:01 +0000

    set up variables

---
+++ m2lab.cpp
@@ -0,0 +1,28 @@
+/*
+Jace C
+csc134
+9/8/25
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //set up the variables
+    double length, width, height; // size of crate
+    double cost_to_make, charge_to_customer; // pricing
+    double profit;
+
+    const double COST_PER_CUBIC_FOOT = 0.23; //relates to cost
+    const double CHARGE_PER_CUBIC_FOOT = 0.5; //relates to charge
+    // get the crate information
+
+
+    //calculate the cost and prices
+
+    //display the results
+
+    return 0; // no errors
+}
