66b98b8b599fde92aaa204513b78fef9e23b97bf
JaceC1328 <caswellj1328@student.faytechcc.edu>
2025-09-17 15:05:16 +0000

    Started m2hw1

---
+++ m2hw1.cpp
@@ -0,0 +1,22 @@
+/*
+CSC134
+m2hw1 gold
+jacec
+9/17/25
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //Question 1
+
+    //Question 2
+
+    //Question 3
+
+    //Question 4
+    cout << "Let's Go " << endl;
+    
+}
