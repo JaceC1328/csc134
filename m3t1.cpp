e51b000578458caa9628e1aea4a767a9c8551f26
JaceC1328 <caswellj1328@student.faytechcc.edu>
2025-09-17 14:32:04 +0000

    set area variables

---
+++ m3t1.cpp
@@ -0,0 +1,30 @@
+/*
+CSC134
+M3T1 Area of Rectangles
+Simple comparison
+jacec
+9/17/25
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // get info on the two rectangles
+    double length1, length2, width1, width2;
+    double area1, area2;
+    cout << "Enter the length and width of the two rectangles." << endl;
+    cout << "First rectangle." << endl;
+    cout << "Length: ";
+    cin >> length1;
+    cout << "Width";
+    cout << endl;
+     cout << "Second rectangle." << endl;
+    cout << "Length: ";
+    cin >> length2;
+    cout << "Width: ";
+     // Calculate the area
+     area1 = length1 * width1;
+     area2 = length2 * width2;
+}
