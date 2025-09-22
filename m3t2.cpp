0b0f1ace58ea358fe79e1984d77e2026c61ea8f4
JaceC1328 <caswellj1328@student.faytechcc.edu>
2025-09-22 14:40:29 +0000

    base skeleton m3t2

---
+++ m3t2.cpp
@@ -0,0 +1,31 @@
+/*
+jacec
+m3t2 -craps part 1
+csc134
+9/22/25
+begining of craps game
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+using namespace std;
+
+// Helper function (todo)
+int roll();
+
+//main
+int main() {
+    int num = roll();
+    cout << num << endl;
+    return 0;
+}
+
+//Define Helper functions
+int roll() {
+    //rolls a 6 sided die
+    //TODO
+    return 6;
+}
